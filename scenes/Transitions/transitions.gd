1beb48b342530086f5209288f3b095e13a0d0595
salty64 <cedric.arrabie@univ-pau.fr>
2024-05-04 11:24:11 +0200

    Transitions exploration

--- scenes/Transitions/transitions.gd
+++ scenes/Transitions/transitions.gd
@@ -4,6 +4,7 @@ extends CanvasLayer
 @onready var Transition_Animate: AnimationPlayer = $AnimationPlayer
 
 
+
 const CIRCLE_MASK = preload("res://scenes/Transitions/Shaders/Circle_mask.gdshader")
 const SQUARE_MASK = preload("res://scenes/Transitions/Shaders/Square_mask.gdshader")
 const SCREEN_MASK = preload("res://scenes/Transitions/Shaders/Screen_mask.gdshader")
@@ -12,24 +13,16 @@ const SWIPE_MASK = preload("res://scenes/Transitions/Shaders/Swipe_Mask.gdshader
 const DIAMOND = preload("res://scenes/Transitions/Shaders/Diamond.gdshader")
 const LINE = preload("res://scenes/Transitions/Shaders/Line.gdshader")
 
+
 func _ready():
-	transition("Swipe")
-	await Transition_Animate.animation_finished
-	transition("Square")
-	await Transition_Animate.animation_finished
-	transition("Circle")
-	await Transition_Animate.animation_finished
-	transition("Sprite")
-	await Transition_Animate.animation_finished
-	transition("Diamond")
-	await Transition_Animate.animation_finished
-	transition("Line")
+	transition("Circle", false)
+
 
 func screen_ratio ():
 	var x = $TextureRect.get_size() 
 	return x[1]/x[0]
 
-func transition (Transition_Name:String) :
+func transition (Transition_Name:String, Reverse :bool) :
 	match Transition_Name:
 
 		"Swipe":
@@ -51,4 +44,9 @@ func transition (Transition_Name:String) :
 		"Line":
 			texture.material.shader = LINE
 
-	$AnimationPlayer.play(Transition_Name)
+	if !Reverse : 
+		$AnimationPlayer.play(Transition_Name)
+	else :
+		$AnimationPlayer.play_backwards(Transition_Name)
+	await Transition_Animate.animation_finished
+
