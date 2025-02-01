59c59f1b012060013a2d3a0db9d797de124807af
theludovyc <theludovyc@gmail.com>
2025-02-01 11:21:17 +0100

    fix #21
    Make MainMenu script easier to extends

--- scenes/MainMenu/MainMenu.gd
+++ scenes/MainMenu/MainMenu.gd
@@ -3,25 +3,9 @@ extends Control
 
 var sub_menu
 
-# Position : margin
-@export_enum("Left","Right") var Template_position:String="Left"
-
-@onready var header_margin = $VBoxContainer/HeaderMargin
-
 @onready var continue_button = %ContinueButton
 @onready var play_button = %PlayButton
 @onready var load_button = %LoadButton
-@onready var option_button = %OptionsButton
-@onready var credit_button = %CreditsButton
-@onready var exit_button = %ExitButton
-@onready var menu_button_list = [
-	continue_button,
-	play_button,
-	load_button,
-	option_button,
-	credit_button,
-	exit_button
-]
 
 @onready var load_save_menu = %LoadSaveMenu
 @onready var option_menu = %OptionsMenu
@@ -45,16 +29,6 @@ func _close_sub_menu():
 	sub_menu.hide()
 	sub_menu = null
 
-
-func _event_is_mouse_button_released(event : InputEvent):
-	return event is InputEventMouseButton and not event.is_pressed()
-
-func _event_skips_intro(event : InputEvent):
-	return event.is_action_released("ui_accept") or \
-		event.is_action_released("ui_select") or \
-		event.is_action_released("ui_cancel") or \
-		_event_is_mouse_button_released(event)
-
 func _ready():
 	if OS.has_feature("web"):
 		%ExitButton.hide()
@@ -62,15 +36,6 @@ func _ready():
 		confirm_popup.get_ok_button().pressed.connect(_on_exit_confirmed)
 		
 	%VersionNumber.text = "version : " + str(ProjectSettings.get_setting("application/config/version", ""))
-	
-	if Template_position=="Right" :
-		header_margin.size_flags_horizontal  = SIZE_SHRINK_END
-		#menu_margin.size_flags_horizontal = SIZE_SHRINK_END
-		var hbox = $VBoxContainer/MarginContainer/HBoxContainer
-		hbox.move_child(hbox.get_child(1),0)
-		hbox.alignment = BoxContainer.ALIGNMENT_END
-		for button in menu_button_list:
-			button.alignment = HORIZONTAL_ALIGNMENT_RIGHT
 	
 	if RGT_Globals.loading_scene_setting.is_empty():
 		play_button.hide()
